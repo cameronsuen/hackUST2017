08d5994534a45b69d5432bd449aba9ff5efb3fda
tensencond <wslamal@connect.ust.hk>
2017-04-22 16:38:17 +0800

    fblogin

--- mobile-app/src/pages/fblogin/home.ts
+++ mobile-app/src/pages/fblogin/home.ts
@@ -1,24 +1,11 @@
-import { OnInit, Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { Geolocation } from '@ionic-native/geolocation';
-import { googlemaps } from 'googlemaps';
 
-/**
- * Generated class for the Home page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
-
-declare var google;
-
-//@IonicPage()
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html',
+  selector: 'page-fbLogin',
+  templateUrl: 'fbLogin.html',
 })
 
-export class HomePage implements OnInit {
+export class LoginPage {
 
     autocompleteItems: any;
     autocomplete: any;
@@ -32,57 +19,15 @@ export class HomePage implements OnInit {
 
     }
 
-    ngOnInit() {
-        this.acService = new google.maps.places.AutocompleteService();        
-        this.autocompleteItems = [];
-        this.autocomplete = {
-            query: ''
-        };  
-    }
 
     ionViewDidLoad() {
         this.loadMap();
-        console.log('ionViewDidLoad Home');
+        console.log('ionViewDidLoad fbLogin');
     }
 
-    updateSearch() {
-        console.log('modal > updateSearch');
-        if (this.autocomplete.query == '') {
-            this.autocompleteItems = [];
-            return;
-        }
-        let self = this; 
-        let config = { 
-            //types:  ['geocode'], // other types available in the API: 'establishment', 'regions', and 'cities'
-            input: this.autocomplete.query, 
-            componentRestrictions: {  } 
-        }
-
-        this.acService.getPlacePredictions(config, function (predictions, status) {
-            console.log('modal > getPlacePredictions > status > ', status);
-            self.autocompleteItems = [];            
-            predictions.forEach(function (prediction) {              
-                self.autocompleteItems.push(prediction);
-            });
-        });
-    }
-    
-    loadMap(){
- 
-        this.geolocation.getCurrentPosition().then((position) => {
-            let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-     
-            let mapOptions = {
-              center: latLng,
-              zoom: 15,
-              mapTypeId: google.maps.MapTypeId.ROADMAP
-            }
-         
-            this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-         }, (err) => {
-             console.log(err);
-         });
-     
-    }
-
+	
+	
+	
+	
 }
+	
