8d3f33cbf02726af21ad7f74972643e154750a17
Cameron Suen <cameron10913@gmail.com>
2017-04-23 01:26:14 +0800

    Implement facebook login

--- mobile-app/src/app/app.component.ts
+++ mobile-app/src/app/app.component.ts
@@ -4,16 +4,30 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/fblogin/home';
 
 import { CurrencyModal } from '../components/currency-modal/currency-modal';
+
+import firebase from 'firebase';
 
 @Component({
     templateUrl: 'app.html',
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = LoginPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+
+  var config = {
+        apiKey: "AIzaSyC6LWXTXeO6axRe02xAEWu9CvDMr0EqWjo",
+        authDomain: "hackust2017-1492830202855.firebaseapp.com",
+        databaseURL: "https://hackust2017-1492830202855.firebaseio.com",
+        projectId: "hackust2017-1492830202855",
+        storageBucket: "hackust2017-1492830202855.appspot.com",
+        messagingSenderId: "1030672296146"
+  };
+  firebase.initializeApp(config);
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
--- mobile-app/src/app/app.module.ts
+++ mobile-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Facebook } from '@ionic-native/facebook';
 
 import { Geolocation } from '@ionic-native/geolocation';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/fblogin/home';
 
 import { footbarComponent } from '../pages/footbar/footbar';
 import { CurrencyModal } from '../components/currency-modal/currency-modal';
@@ -17,6 +19,7 @@ import { CurrencyModal } from '../components/currency-modal/currency-modal';
   declarations: [
     MyApp,
     HomePage,
+    LoginPage,
     footbarComponent,
     CurrencyModal
   ],
@@ -27,12 +30,14 @@ import { CurrencyModal } from '../components/currency-modal/currency-modal';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
     Geolocation,
+    Facebook,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
