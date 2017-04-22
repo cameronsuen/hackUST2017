8f5d567fe218034efbbd12506989155c78fc99bc
Cameron Suen <cameron10913@gmail.com>
2017-04-22 13:31:34 +0800

    Implemented places search

--- mobile-app/src/pages/home/home.ts
+++ mobile-app/src/pages/home/home.ts
@@ -1,6 +1,7 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { OnInit, Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
+import { googlemaps } from 'googlemaps';
 
 /**
  * Generated class for the Home page.
@@ -17,35 +18,71 @@ declare var google;
   templateUrl: 'home.html',
 })
 
-export class HomePage {
+export class HomePage implements OnInit {
+
+    autocompleteItems: any;
+    autocomplete: any;
+    acService:any;
+    placesService: any; 
 
     @ViewChild('map') mapElement: ElementRef;
     map: any;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public geolocation: Geolocation) {
 
-  }
+    constructor(public navCtrl: NavController, public navParams: NavParams, public geolocation: Geolocation) {
 
-  ionViewDidLoad() {
-    this.loadMap();
-    console.log('ionViewDidLoad Home');
-  }
+    }
 
- loadMap(){
- 
-     this.geolocation.getCurrentPosition().then((position) => {
-        let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
- 
-        let mapOptions = {
-          center: latLng,
-          zoom: 15,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
+    ngOnInit() {
+        this.acService = new google.maps.places.AutocompleteService();        
+        this.autocompleteItems = [];
+        this.autocomplete = {
+            query: ''
+        };  
+    }
+
+    ionViewDidLoad() {
+        this.loadMap();
+        console.log('ionViewDidLoad Home');
+    }
+
+    updateSearch() {
+        console.log('modal > updateSearch');
+        if (this.autocomplete.query == '') {
+            this.autocompleteItems = [];
+            return;
         }
+        let self = this; 
+        let config = { 
+            //types:  ['geocode'], // other types available in the API: 'establishment', 'regions', and 'cities'
+            input: this.autocomplete.query, 
+            componentRestrictions: {  } 
+        }
+
+        this.acService.getPlacePredictions(config, function (predictions, status) {
+            console.log('modal > getPlacePredictions > status > ', status);
+            self.autocompleteItems = [];            
+            predictions.forEach(function (prediction) {              
+                self.autocompleteItems.push(prediction);
+            });
+        });
+    }
+    
+    loadMap(){
+ 
+        this.geolocation.getCurrentPosition().then((position) => {
+            let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
      
-        this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-     }, (err) => {
-         console.log(err);
-     });
+            let mapOptions = {
+              center: latLng,
+              zoom: 15,
+              mapTypeId: google.maps.MapTypeId.ROADMAP
+            }
+         
+            this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+         }, (err) => {
+             console.log(err);
+         });
      
-  }
+    }
 
 }
